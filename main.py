def2828df929a08cfe92759228a195e8fefbb5f9
Eric <41165769+3lo@users.noreply.github.com>
2020-01-02 21:28:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ pygame.display.set_caption("Tic Tac Toe")  # title of window
 clock = pygame.time.Clock()  # frames per second (game clock)
 turn = True
 
+game_over = True
+
 
 def text_objects(text, font):
     textSurface = font.render(text, True, black)
@@ -46,14 +48,14 @@ def scoreboard(score1, score2):
     smallText = pygame.font.Font("freesansbold.ttf", 20)
     textSurf, textRect = text_objects("Player 1: " + str(player1_score), smallText)
     textRect.center = (53, 12)
-    pygame.draw.rect(screen, red, pygame.Rect(0, 0, 320, 20))
+    pygame.draw.rect(screen, red, pygame.Rect(0, 0, 320, 25))
     screen.blit(textSurf, textRect)
 
     player2_score = player2_score + score2
     smallText = pygame.font.Font("freesansbold.ttf", 20)
     textSurf, textRect = text_objects("Player 2: " + str(player2_score), smallText)
     textRect.center = (53, 35)
-    pygame.draw.rect(screen, red, pygame.Rect(0, 20, 320, 25))
+    pygame.draw.rect(screen, red, pygame.Rect(0, 25, 320, 25))
     screen.blit(textSurf, textRect)
 
 
@@ -90,20 +92,6 @@ def set_player(border):
         check_winner(wins)
 
 
-# data storage for winner checking
-# horizontal rows
-top_row = []
-middle_row = []
-bottom_row = []
-# veritcal rows
-first_column = []
-sec_column = []
-third_column = []
-# diagonals
-forward_slash = []  # /
-back_slash = []  # \
-
-
 def winner(pos):
     area = pos[0][0], pos[1][0], pos[2][0]
     win = pos[0][1], pos[1][1], pos[2][1]
@@ -117,12 +105,20 @@ def winner(pos):
         game_finished(area)
     elif len(border_list) == 9 and "X" in win and "O" in win:
         print("The game is a tie!")
+        game_finished(None)
 
 
 def game_finished(area):
     print("The game is over!")
-    for i in range(len(area)):
-        pygame.draw.rect(screen, cyan, area[i], 3)
+    if area is None:
+        pass
+    else:
+        for i in range(len(area)):
+            pygame.draw.rect(screen, cyan, area[i], 2)
+        pygame.display.update()
+        pygame.time.delay(2500)
+    global game_over
+    game_over = True
 
 
 def check_winner(data):
@@ -166,58 +162,112 @@ def check_winner(data):
             winner(back_slash)
 
 
-topRowLeftTile = Tiles((0, 50, 106, 63), black, None)
-topRowMidTile = Tiles((107, 50, 106, 63), black, None)
-topRowRightTile = Tiles((214, 50, 106, 63), black, None)
-
-midRowLeftTile = Tiles((0, 114, 106, 63), black, None)
-midRowMidTile = Tiles((107, 114, 106, 63), black, None)
-midRowRightTile = Tiles((214, 114, 106, 63), black, None)
-
-botRowLeftTile = Tiles((0, 178, 106, 63), black, None)
-botRowMidTile = Tiles((107, 178, 106, 63), black, None)
-botRowRightTile = Tiles((214, 178, 106, 63), black, None)
+def is_turn():
+    global turn
+    if turn is True:
+        turn = False
+    else:
+        turn = True
+    return turn
 
 
 def clicked():
+    global turn
     if 0 + 106 > mouse[0] > 0 and 50 + 63 > mouse[1] > 50:  # inside the area of square1 - mouse = (x, y)
-        set_player(topRowLeftTile)
+        if topRowLeftTile.player is None:
+            set_player(topRowLeftTile)
+        else:
+            is_turn()
     elif 0 + 106 > mouse[0] > 0 and 114 + 63 > mouse[1] > 114:
-        set_player(midRowLeftTile)
+        if midRowLeftTile.player is None:
+            set_player(midRowLeftTile)
+        else:
+            is_turn()
     elif 0 + 106 > mouse[0] > 0 and 178 + 63 > mouse[1] > 178:
-        set_player(botRowLeftTile)
+        if botRowLeftTile.player is None:
+            set_player(botRowLeftTile)
+        else:
+            is_turn()
     elif 107 + 106 > mouse[0] > 107 and 50 + 63 > mouse[1] > 50:
-        set_player(topRowMidTile)
+        if topRowMidTile.player is None:
+            set_player(topRowMidTile)
+        else:
+            is_turn()
     elif 107 + 106 > mouse[0] > 107 and 114 + 63 > mouse[1] > 114:
-        set_player(midRowMidTile)
+        if midRowMidTile.player is None:
+            set_player(midRowMidTile)
+        else:
+            is_turn()
     elif 107 + 106 > mouse[0] > 107 and 178 + 63 > mouse[1] > 178:
-        set_player(botRowMidTile)
+        if botRowMidTile.player is None:
+            set_player(botRowMidTile)
+        else:
+            is_turn()
     elif 214 + 106 > mouse[0] > 214 and 50 + 63 > mouse[1] > 50:
-        set_player(topRowRightTile)
+        if topRowRightTile.player is None:
+            set_player(topRowRightTile)
+        else:
+            is_turn()
     elif 214 + 106 > mouse[0] > 214 and 114 + 63 > mouse[1] > 114:
-        set_player(midRowRightTile)
+        if midRowRightTile.player is None:
+            set_player(midRowRightTile)
+        else:
+            is_turn()
     elif 214 + 106 > mouse[0] > 214 and 178 + 63 > mouse[1] > 178:
-        set_player(botRowRightTile)
+        if botRowRightTile.player is None:
+            set_player(botRowRightTile)
+        else:
+            is_turn()
+    else:
+        is_turn()
 
 
-screen.fill(white)
-pygame.draw.rect(screen, red, pygame.Rect(0, 0, 320, 50))  # x, y, width, height - scoreboard
-background()
 scoreboard(0, 0)
 
 while 1:
     mouse = pygame.mouse.get_pos()  # gets mouse position
     click = pygame.mouse.get_pressed()  # gets mouse clicks
 
-    for event in pygame.event.get():  # gets any event that happens (mouse movement, clicks, key presses)
-        if event.type == pygame.QUIT:  # exited the window quits program
-            sys.exit()
-        elif event.type == pygame.MOUSEBUTTONDOWN and mouse[1] > 50:  # add condition for areas that have been clicked
-            clicked()
-            if turn is True:
-                turn = False
-            else:
-                turn = True
+    if game_over:
+        pygame.draw.rect(screen, white, (0, 50, 320, 240))
+
+        topRowLeftTile = Tiles((0, 50, 106, 63), black, None)
+        topRowMidTile = Tiles((107, 50, 106, 63), black, None)
+        topRowRightTile = Tiles((214, 50, 106, 63), black, None)
+
+        midRowLeftTile = Tiles((0, 114, 106, 63), black, None)
+        midRowMidTile = Tiles((107, 114, 106, 63), black, None)
+        midRowRightTile = Tiles((214, 114, 106, 63), black, None)
+
+        botRowLeftTile = Tiles((0, 178, 106, 63), black, None)
+        botRowMidTile = Tiles((107, 178, 106, 63), black, None)
+        botRowRightTile = Tiles((214, 178, 106, 63), black, None)
+
+        background()
+        border_list = []
+
+        # data storage for winner checking
+        top_row = []  # horizontal rows
+        middle_row = []
+        bottom_row = []
+        first_column = []  # veritcal rows
+        sec_column = []
+        third_column = []
+        forward_slash = []   # diagonals
+        back_slash = []
+
+        game_over = False
+        turn = True
+    else:
+        for event in pygame.event.get():  # gets any event that happens (mouse movement, clicks, key presses)
+            if event.type == pygame.QUIT:  # exited the window quits program
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN and mouse[1] > 50:
+                clicked()
+                if turn is True:
+                    turn = False
+                else:
+                    turn = True
 
     pygame.display.update()  # or pygame.display.flip()
     clock.tick(30)  # frames per second
